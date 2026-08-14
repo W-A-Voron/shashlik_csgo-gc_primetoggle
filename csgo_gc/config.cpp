bc90878c08710fd09489cedb7f02fffa41eff42b
W-A-Voron <voron.matvey2013@gmail.com>
2026-08-14 17:55:01 +0700

    Update config.cpp

--- csgo_gc/config.cpp
+++ csgo_gc/config.cpp
@@ -3,29 +3,7 @@
 
 GCConfig::GCConfig()
 {
-    // Включение удаления использованных предметов (кейсы, ключи, ярлыки)
-    m_destroyUsedItems = true;
-
-    // Выключение принудительного выпадения самых редких предметов
-    m_forceMaxRarity = false;
-
-    // Включение рандомизации износа (float)
-    m_randomizeFloat = true;
-
-    // Кулдаун по умолчанию (0 = отключён)
-    m_competitiveCooldownSeconds = 0;
-
-    // Реалистичные веса выпадения для каждого уровня редкости
-    m_rarityWeights = {
-        { ItemSchema::RarityCommon,   10000000 }, // 10 млн  (обычные)
-        { ItemSchema::RarityUncommon, 2000000  }, // 2 млн   (необычные)
-        { ItemSchema::RarityRare,     400000   }, // 400 тыс (редкие)
-        { ItemSchema::RarityMythical, 80000    }, // 80 тыс  (мифические)
-        { ItemSchema::RarityLegendary,16000    }, // 16 тыс  (легендарные)
-        { ItemSchema::RarityAncient,  3200     }, // 3.2 тыс (древние)
-        { ItemSchema::RarityImmortal, 640      }, // 640     (бессмертные - ножи)
-        { ItemSchema::RarityUnusual,  1280     }, // 1280    (необычные - ножи с паттерном)
-    };
+    // Все поля инициализированы прямо в заголовке, здесь ничего не нужно делать.
 }
 
 float GCConfig::GetRarityWeight(uint32_t rarity) const
