de63a457b210de1d3bbb203899a158edaae85682
W-A-Voron <voron.matvey2013@gmail.com>
2026-08-14 17:54:33 +0700

    Update config.h

--- csgo_gc/config.h
+++ csgo_gc/config.h
@@ -16,50 +16,86 @@ public:
     void ReloadFromFile() {} // Файлы не используются
 
     // === ЖЁСТКИЕ НАСТРОЙКИ ===
-    uint32_t AppIdOverride() const { return 4465480; }
-    bool ShowCsgoGCServersOnly() const { return true; }
+    uint32_t AppIdOverride() const { return m_appIdOverride; }
+    bool ShowCsgoGCServersOnly() const { return m_showCsgoGCServersOnly; }
 
-    // === РЕАЛИСТИЧНЫЙ РАНГ (Золотая Звезда 3 / Master Guardian 1) ===
-    RankId CompetitiveRank() const { return RankMasterGuardian1; }
-    int CompetitiveWins() const { return 98; }
+    // === РЕАЛИСТИЧНЫЙ РАНГ ===
+    RankId CompetitiveRank() const { return m_competitiveRank; }
+    int CompetitiveWins() const { return m_competitiveWins; }
 
-    RankId WingmanRank() const { return RankMasterGuardian1; }
-    int WingmanWins() const { return 42; }
+    RankId WingmanRank() const { return m_wingmanRank; }
+    int WingmanWins() const { return m_wingmanWins; }
 
-    DangerZoneRankId DangerZoneRank() const { return DangerZoneRankScoutElite; }
-    int DangerZoneWins() const { return 15; }
+    DangerZoneRankId DangerZoneRank() const { return m_dangerZoneRank; }
+    int DangerZoneWins() const { return m_dangerZoneWins; }
 
     // === НАСТРОЙКИ ИГРЫ ===
-    bool ForceMaxRarity() const { return false; }       // Реальные шансы выпадения
-    bool DestroyUsedItems() const { return true; }      // Предметы исчезают после использования
-    bool RandomizeFloat() const { return true; }
+    bool ForceMaxRarity() const { return m_forceMaxRarity; }
+    bool DestroyUsedItems() const { return m_destroyUsedItems; }
+    bool RandomizeFloat() const { return m_randomizeFloat; }
 
-    bool VacBanned() const { return false; }
-    bool HasPrime() const { return true; }
-    uint32_t CompetitiveCooldownSeconds() const { return 0; }
+    bool VacBanned() const { return m_vacBanned; }
+    bool HasPrime() const { return m_hasPrime; }
+    uint32_t CompetitiveCooldownSeconds() const { return m_competitiveCooldownSeconds; }
 
-    // === РЕАЛИСТИЧНЫЕ ДАННЫЕ СТАТИСТИКИ ===
-    int CommendedFriendly() const { return 12; }   // Дружелюбный
-    int CommendedTeaching() const { return 5; }    // Наставник
-    int CommendedLeader() const { return 3; }      // Лидер
-    int Level() const { return 28; }               // 28 уровень
-    int Xp() const { return 3200; }                // 3200/5000 до следующего уровня
+    // === РЕАЛИСТИЧНЫЕ ДАННЫЕ ===
+    int CommendedFriendly() const { return m_commendedFriendly; }
+    int CommendedTeaching() const { return m_commendedTeaching; }
+    int CommendedLeader() const { return m_commendedLeader; }
+    int Level() const { return m_level; }
+    int Xp() const { return m_xp; }
 
-    std::string Country() const { return "RU"; }
-    int Currency() const { return 3; } // RUB
+    std::string Country() const { return m_country; }
+    int Currency() const { return m_currency; }
 
-    // === ШАНСЫ ВЫПАДЕНИЯ (как в реальном CS:GO) ===
     float GetRarityWeight(uint32_t rarity) const;
-
-    std::vector<int> GetFriends() const { return { 1140104601, 7656119801234567, 7656119807654321 }; };
+    std::vector<int> GetFriends() const { return m_friends; };
 
 private:
-    bool m_forceMaxRarity;
-    bool m_destroyUsedItems;
-    bool m_randomizeFloat;
-    uint32_t m_competitiveCooldownSeconds;
+    // Настройки
+    uint32_t m_appIdOverride = 4465480;
+    bool m_showCsgoGCServersOnly = true;
 
-    std::vector<RarityWeight> m_rarityWeights;
+    // Ранги
+    RankId m_competitiveRank = RankMasterGuardian1;
+    int m_competitiveWins = 98;
+    RankId m_wingmanRank = RankMasterGuardian1;
+    int m_wingmanWins = 42;
+    DangerZoneRankId m_dangerZoneRank = DangerZoneRankScoutElite;
+    int m_dangerZoneWins = 15;
+
+    // Игровые настройки
+    bool m_forceMaxRarity = false;
+    bool m_destroyUsedItems = true;
+    bool m_randomizeFloat = true;
+    bool m_vacBanned = false;
+    bool m_hasPrime = true;
+    uint32_t m_competitiveCooldownSeconds = 0;
+
+    // Статистика
+    int m_commendedFriendly = 12;
+    int m_commendedTeaching = 5;
+    int m_commendedLeader = 3;
+    int m_level = 28;
+    int m_xp = 3200;
+
+    std::string m_country = "RU";
+    int m_currency = 3;
+
+    // Друзья
+    std::vector<int> m_friends = { 1140104601, 7656119801234567, 7656119807654321 };
+
+    // Веса (используем суффикс U для uint32_t, чтобы избежать сужающих преобразований)
+    std::vector<RarityWeight> m_rarityWeights = {
+        { ItemSchema::RarityCommon, 10000000U },
+        { ItemSchema::RarityUncommon, 2000000U },
+        { ItemSchema::RarityRare, 400000U },
+        { ItemSchema::RarityMythical, 80000U },
+        { ItemSchema::RarityLegendary, 16000U },
+        { ItemSchema::RarityAncient, 3200U },
+        { ItemSchema::RarityImmortal, 640U },
+        { ItemSchema::RarityUnusual, 1280U },
+    };
 };
 
 GCConfig &GetConfig();
