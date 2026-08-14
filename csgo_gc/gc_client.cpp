d1639662c835e1c5f2d1d5a5a0a8ca26f1eb3f9e
W-A-Voron <voron.matvey2013@gmail.com>
2026-08-15 06:42:37 +0700

    Update gc_client.cpp

--- csgo_gc/gc_client.cpp
+++ csgo_gc/gc_client.cpp
@@ -1162,8 +1162,3 @@ void ClientGC::SendVerdictToCloudflare(const CMsgGCCStrike15_v2_PlayerOverwatchC
 
     http->SendHTTPRequest(hRequest, nullptr);
 }
-
-uint32_t ClientGC::EffectiveAccountId() const
-{
-    return AccountId();
-}
